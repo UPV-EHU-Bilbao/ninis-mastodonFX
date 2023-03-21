604c22b4f53a75c6f006b6bc922b9df0c22ef911
s0ri23 <sori200123@gmail.com>
2023-03-21 16:33:44 +0100

    followers uwu

--- src/main/java/eus/ehu/sprint1/ClientApplication.java
+++ src/main/java/eus/ehu/sprint1/ClientApplication.java
@@ -3,10 +3,9 @@ package eus.ehu.sprint1;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import java.io.IOException;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 
 public class ClientApplication extends Application {
--- src/main/java/eus/ehu/sprint1/FollowersController.java
+++ src/main/java/eus/ehu/sprint1/FollowersController.java
@@ -1,6 +1,25 @@
 package eus.ehu.sprint1;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
-public class FollowersController implements FxController{
+public class FollowersController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextArea followersArea;
+
+    @FXML
+    void initialize() {
+        assert followersArea != null : "fx:id=\"followersArea\" was not injected: check your FXML file 'followers.fxml'.";
+
+    }
 
 }
