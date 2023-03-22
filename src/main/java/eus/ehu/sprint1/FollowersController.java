96438b0399b27c71d6da376315e9c34e890317b3
s0ri23 <sori200123@gmail.com>
2023-03-22 14:20:41 +0100

    followers 1.1.3

--- src/main/java/eus/ehu/sprint1/FollowersController.java
+++ src/main/java/eus/ehu/sprint1/FollowersController.java
@@ -5,6 +5,11 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 
+import social.bigbone.MastodonClient;
+import social.bigbone.api.exception.BigBoneRequestException;
+
+import static social.bigbone.api.method.TimelineMethods.StatusOrigin.LOCAL_AND_REMOTE;
+
 public class FollowersController {
 
     @FXML
@@ -17,9 +22,18 @@ public class FollowersController {
     private TextArea followersArea;
 
     @FXML
-    void initialize() {
-        assert followersArea != null : "fx:id=\"followersArea\" was not injected: check your FXML file 'followers.fxml'.";
+    void initialize() throws BigBoneRequestException {
+        final String instance = "@alaitz19@mastodon.social";
 
+        // Instantiate client
+        final MastodonClient client = new MastodonClient.Builder(instance).accessToken(accessToken).build();
+
+        // Print timeline statuses
+        client.timelines().getPublicTimeline(LOCAL_AND_REMOTE).doOnJson(
+                json -> {
+                    followersArea.setText(json.toString());
+                }
+        ).execute();
     }
 
 }
